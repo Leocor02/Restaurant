099c59a4534136c6a0cc3b9d93af76b64cb8ff15
Leonardo Cortes <cortesleo02@gmail.com>
2022-11-24 21:25:02 -0600

    Agregar archivos de proyecto.

---
+++ Restaurant/Restaurant/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Restaurant.Services;
+using Restaurant.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Restaurant
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            //Vista en la cual inicia el app
+            MainPage = new NavigationPage(new AppLoginPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Restaurant/Restaurant/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Restaurant.Models;
+using Restaurant.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Restaurant.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
