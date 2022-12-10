e3119e363262a531a4ada873cbd9e6f1888b1e19
JorgeRochaA <jor2019.cr@gmail.com>
2022-12-10 16:16:00 -0600

    feat : add pages for managin Employees

---
+++ Restaurant/Restaurant/Views/Admi/Manage EmployeesPages/ShowEmployees.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Restaurant.Views.Admi.Manage_Employees
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShowEmployees : ContentPage
+    {
+        public ShowEmployees()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Restaurant/Restaurant/Views/HomeAdmin.xaml.cs
+++ Restaurant/Restaurant/Views/HomeAdmin.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Restaurant.Models;
+using Restaurant.Models.DTO;
+using Restaurant.ViewModels;
+using Restaurant.Views.Admi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +19,30 @@ namespace Restaurant.Views
         public HomeAdmin()
         {
             InitializeComponent();
+            this.Title = "Bienvenido! " + GlobalItems.GlobalUser.Name;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert(":v",GlobalItems.GlobalUser.Name,"ok");
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            //btn Administrar empleados
+            await Navigation.PushAsync(new ManageEmployeesPage());
+        }
+
+        private async void Button_Clicked_2(object sender, EventArgs e)
+        {
+            //btn Administrar Mesas
+            await Navigation.PushAsync(new ManageTablesPage());
+        }
+
+        private async void Button_Clicked_3(object sender, EventArgs e)
+        {
+            //btn Ver Reservaciones
+            await Navigation.PushAsync(new ShowRevervationsPage());
         }
     }
 }
